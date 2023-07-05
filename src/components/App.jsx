b15b339491dacd9c52c8f6e7298203323388cded
Rubesh K R <rupesh.maxpain@gmail.com>
2023-07-05 22:32:43 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Form from "./Form";
+
+var isLoggedIn = false;
+
+function renderConditionally() {
+  if (isLoggedIn === true) {
+    return <h1>Hello</h1>;
+  } else {
+    return <Form />;
+  }
+}
+
+function App() {
+  return <div className="container">{renderConditionally()}</div>;
+}
+
+export default App;
